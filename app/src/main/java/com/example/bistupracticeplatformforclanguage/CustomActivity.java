e5354a37f3ac030bc5d63aae4edb64a4332de4d0
st01tyy <44644324+st01tyy@users.noreply.github.com>
2019-11-08 17:52:48 +0800

    Delete CustomActivity.java

--- app/src/main/java/com/example/bistupracticeplatformforclanguage/CustomActivity.java
+++
@@ -1,8 +0,0 @@
-package com.example.bistupracticeplatformforclanguage;
-
-import androidx.appcompat.app.AppCompatActivity;
-
-abstract public class CustomActivity extends AppCompatActivity
-{
-    abstract public void updateAnswer(String ans);
-}
